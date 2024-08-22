4cf2926a7c638997609eb5dff0168ded7040259d
unknown <sknizm007@gmail.com>
2024-08-22 20:05:04 +0530

    ol

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../Components/Hero/Hero'
+import About from '../../Components/About/About'
+import Testimonial from '../../Components/Testimonial/Testimonial'
 
 const Home = () => {
   return (
     <div className='home'>
       <Hero/>
+      <About/>
+      <Testimonial/>
+      
     </div>
   )
 }
